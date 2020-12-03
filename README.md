990e6dad096a9ce397253f31ae558f5a4e1f44a5
Matheus Gonçalves <matheusgt4@hotmail.com>
2020-12-02 23:49:42 -0300

    Finalizando

--- README.md
+++ README.md
@@ -11,3 +11,8 @@ Trabalho final de LP4
 Utilize o `atividade.sql` para obter o esquema do banco.
 
 As rotas foram pensadas para utilizar o **Apache**, caso quebrem utilize o caminho puro.
+
+## Feito por:
+
+Matheus Gonçalves Mourão Rodrigues
+Julia Gabrielle Luciano Nunes
--- components/footer.php
+++ components/footer.php
@@ -1,5 +1,5 @@
 <footer class="jumbotron text-center mb-0" id="footer">
-      <p><strong>Matheus Gonçalves Mourão Rodrigues</strong></p>
+      <p><strong>Matheus Gonçalves Mourão Rodrigues</strong> e <strong>Julia Gabrielle Luciano Nunes</strong></p>
     </footer>
   </body>
 </html>
